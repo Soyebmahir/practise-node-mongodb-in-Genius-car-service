a03ccb80d4aced560aaeaf20b881d0cb073f1d25
Soyeb <tssoyeb@gmail.com>
2022-04-23 19:22:05 +0600

    initial firebase setup

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+/* // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
@@ -19,4 +19,25 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
+export default auth; */
+// Import the functions you need from the SDKs you need
+import {getAuth} from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAL4GcwcANneMa4-UaLa1dRl7kR6Sw9O10",
+  authDomain: "node-mongodb-in-car-service.firebaseapp.com",
+  projectId: "node-mongodb-in-car-service",
+  storageBucket: "node-mongodb-in-car-service.appspot.com",
+  messagingSenderId: "928301665762",
+  appId: "1:928301665762:web:423fe432712ee4061efdbb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth =getAuth(app)
 export default auth;
